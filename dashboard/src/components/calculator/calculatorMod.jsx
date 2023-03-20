b38129fd5239516695d133a7f08f76c88b368fad
Jamie Jenks <jamielynjenks@gmail.com>
2023-03-20 13:06:01 -0600

    started writing the functions for the calculator. It's more involved than I originally thought lol

--- dashboard/src/components/calculator/calculatorMod.jsx
+++ dashboard/src/components/calculator/calculatorMod.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const emptyNumber = {number: 0, nextOperation: undefined}
 
 function CalculatorMod(){
+    //Calculation functions
+    const [currentNumber, setCurrentNumber] = useState(emptyNumber);
+    const [numbers, setNumbers] = useState([{}]);
+
+    function addNumbers(e){
+        setCurrentNumber({...currentNumber, number: e.tarrget.name});
+    }
+
+    function addOperation(e){
+        setCurrentNumber({...currentNumber, nextOperation: e.target.name});
+        setNumbers([...numbers, {...currentNumber}]);
+        setCurrentNumber(emptyNumber);
+    }
+
+    function allEquals(){
+
+    }
     
     return (<div>This Will Become A Calculator</div>)
 }
