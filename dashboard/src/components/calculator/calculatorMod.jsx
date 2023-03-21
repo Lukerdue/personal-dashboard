df6c9ea07d2b0e8e603d01671baf6c874e099aaf
Jamie Jenks <jamielynjenks@gmail.com>
2023-03-21 12:05:41 -0600

    Added all the buttons and scaffolding for a calculator

--- dashboard/src/components/calculator/calculatorMod.jsx
+++ dashboard/src/components/calculator/calculatorMod.jsx
@@ -18,10 +18,87 @@ function CalculatorMod(){
     }
 
     function allEquals(){
-
+        //TODO: figure out how to add all the numbers together lol
     }
     
-    return (<div>This Will Become A Calculator</div>)
+    return (<div>
+        <div className="inner">
+            <div className='history'>
+                {/* TODO: Store history in local storage. */}
+            </div>
+            <div className="calculator">
+                <div className="display">
+                    {/* TODO: how to display current number */}
+                </div>
+                <div className="buttons">
+                    <div className="row">
+                        <div className="number all clear">
+                            <p>AC</p>
+                        </div>
+                        <div className="number clear">
+                            <p>C</p>
+                        </div>
+                        <div className="backspace">
+                            <p>^</p>
+                        </div>
+                        <div className="number add">
+                            <p>+</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="number">
+                            <p>7</p>
+                        </div>
+                        <div className="number">
+                            <p>8</p>
+                        </div>
+                        <div className="number">
+                            <p>9</p>
+                        </div>
+                        <div className="number subtract">
+                            <p>-</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="number">
+                            <p>4</p>
+                        </div>
+                        <div className="number">
+                            <p>5</p>
+                        </div>
+                        <div className="number">
+                            <p>6</p>
+                        </div>
+                        <div className="number multiply">
+                            <p>x</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="number">
+                            <p>1</p>
+                        </div>
+                        <div className="number">
+                            <p>2</p>
+                        </div>
+                        <div className="number">
+                            <p>3</p>
+                        </div>
+                        <div className="number divide">
+                            <p>÷</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="number zero">
+                            <p>0</p>
+                        </div>
+                        <div className="number decimal">
+                            <p>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>)
 }
 
 export default CalculatorMod
